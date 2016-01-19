78f10ecfa7bab81583a158ee1bf7aa17ff553f54
Christoph Rauch <smokey42@users.noreply.github.com>
2016-01-19 17:25:16 +0100

    Bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='paipa',
-    version='0.0.1',
+    version='0.0.2',
     description='Python pipeline library. Maori: (noun) pipe.',
     packages=['paipa'],
     tests_require=[
