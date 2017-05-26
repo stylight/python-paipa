1aabb9d82ddb251a25b1cb5beff57cdb2d232f58
Christoph Rauch <smokey42@users.noreply.github.com>
2017-05-26 11:44:16 +0200

    Expose coroutine funcs at top level

--- paipa/__init__.py
+++ paipa/__init__.py
@@ -4,5 +4,7 @@
 Pipeline processing
 """
 from .threaded import AbstractStep, Pipeline, SkipEntry, iterstep, funcstep
+from .coroutines import combine_pipeline, identity_step
 
-__all__ = ["AbstractStep", "Pipeline", "SkipEntry", "iterstep", "funcstep"]
+__all__ = ["AbstractStep", "Pipeline", "SkipEntry", "iterstep", "funcstep",
+           "combine_pipeline", "identity_step"]
