bb045660b3bf2ec7762d7ff4951caa40d3db4860
Christoph Rauch <smokey42@users.noreply.github.com>
2017-06-16 10:45:35 +0200

    export consume function

--- paipa/__init__.py
+++ paipa/__init__.py
@@ -3,10 +3,11 @@
 """
 Pipeline processing
 """
-from .threaded import AbstractStep, Pipeline, SkipEntry, iterstep, funcstep
+from .iterables import consume
 from .coroutines import combine_pipeline, identity_step
+from .threaded import AbstractStep, Pipeline, SkipEntry, iterstep, funcstep
 
 __all__ = ["AbstractStep", "Pipeline", "SkipEntry", "iterstep", "funcstep",
-           "combine_pipeline", "identity_step"]
+           "combine_pipeline", "identity_step", "consume"]
 
 __version__ = "0.3.0"
